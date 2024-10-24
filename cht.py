4f773e44c32e749de6661dbb5211535358fae9b1
Chatbot <134295092+ChatbotMedias24@users.noreply.github.com>
2024-10-24 16:35:24 +0100

    Add files via upload

---
+++ cht.py
@@ -0,0 +1,214 @@
+import streamlit as st
+import openai
+import streamlit as st
+from dotenv import load_dotenv
+import pickle
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.callbacks import get_openai_callback
+import os
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferWindowMemory
+from langchain.prompts import (
+    SystemMessagePromptTemplate,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder
+)
+from streamlit_chat import message  # Importez la fonction message
+import toml
+import docx2txt
+from langchain.memory.chat_message_histories import StreamlitChatMessageHistory
+import docx2txt
+from dotenv import load_dotenv
+if 'previous_question' not in st.session_state:
+    st.session_state.previous_question = []
+
+# Chargement de l'API Key depuis les variables d'environnement
+load_dotenv(st.secrets["OPENAI_API_KEY"])
+
+# Configuration de l'historique de la conversation
+if 'previous_questions' not in st.session_state:
+    st.session_state.previous_questions = []
+
+st.markdown(
+    """
+    <style>
+
+        .user-message {
+            text-align: left;
+            background-color: #E8F0FF;
+            padding: 8px;
+            border-radius: 15px 15px 15px 0;
+            margin: 4px 0;
+            margin-left: 10px;
+            margin-right: -40px;
+            color:black;
+        }
+
+        .assistant-message {
+            text-align: left;
+            background-color: #F0F0F0;
+            padding: 8px;
+            border-radius: 15px 15px 15px 0;
+            margin: 4px 0;
+            margin-left: -10px;
+            margin-right: 10px;
+            color:black;
+        }
+
+        .message-container {
+            display: flex;
+            align-items: center;
+        }
+
+        .message-avatar {
+            font-size: 25px;
+            margin-right: 20px;
+            flex-shrink: 0; /* Empêcher l'avatar de rétrécir */
+            display: inline-block;
+            vertical-align: middle;
+        }
+
+        .message-content {
+            flex-grow: 1; /* Permettre au message de prendre tout l'espace disponible */
+            display: inline-block; /* Ajout de cette propriété */
+}
+        .message-container.user {
+            justify-content: flex-end; /* Aligner à gauche pour l'utilisateur */
+        }
+
+        .message-container.assistant {
+            justify-content: flex-start; /* Aligner à droite pour l'assistant */
+        }
+        input[type="text"] {
+            background-color: #E0E0E0;
+        }
+
+        /* Style for placeholder text with bold font */
+        input::placeholder {
+            color: #555555; /* Gris foncé */
+            font-weight: bold; /* Mettre en gras */
+        }
+
+        /* Ajouter de l'espace en blanc sous le champ de saisie */
+        .input-space {
+            height: 20px;
+            background-color: white;
+        }
+    
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+# Sidebar contents
+textcontainer = st.container()
+with textcontainer:
+    logo_path = "medi.png"
+    logoo_path = "NOTEPRESENTATION.png"
+    st.sidebar.image(logo_path,width=150)
+   
+    
+st.sidebar.subheader("Suggestions:")
+questions = [
+        "Donnez-moi un résumé du rapport ",
+        "Quels sont les principaux objectifs du Projet de Loi de Finances pour l'année 2025 ?",
+        "Quelles mesures sont prévues pour améliorer l'efficacité des services publics en termes de gestion des ressources humaines ?",
+        "Comment le gouvernement prévoit-il de moderniser les processus de recrutement et de gestion du personnel ?",
+        "Comment le rapport envisage-t-il l'utilisation des nouvelles technologies dans la gestion des ressources humaines ?"
+]
+# Initialisation de l'historique de la conversation dans `st.session_state`
+if 'conversation_history' not in st.session_state:
+    st.session_state.conversation_history = StreamlitChatMessageHistory()
+def main():
+    conversation_history = StreamlitChatMessageHistory()  # Créez l'instance pour l'historique
+
+    st.header("PLF2025: Explorez le rapport sur les ressources humaines à travers notre chatbot 💬")
+    
+    # Load the document
+    docx = 'PLF2025- Rapport Ressources Humaines_Fr.docx'
+    
+    if docx is not None:
+        # Lire le texte du document
+        text = docx2txt.process(docx)
+
+        # Afficher toujours la barre de saisie
+        st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
+        selected_questions = st.sidebar.radio("****Choisir :****", questions)
+        # Afficher toujours la barre de saisie
+        query_input = st.text_input("", key="text_input_query", placeholder="Posez votre question ici...", help="Posez votre question ici...")
+        st.markdown('<div class="input-space"></div>', unsafe_allow_html=True)
+
+        if query_input and query_input not in st.session_state.previous_question:
+            query = query_input
+            st.session_state.previous_question.append(query_input)
+        elif selected_questions:
+            query = selected_questions
+        else:
+            query = ""
+
+        if query :
+            st.session_state.conversation_history.add_user_message(query) 
+            if "Donnez-moi un résumé du rapport" in query:
+                summary="""Le rapport sur les ressources humaines du Projet de Loi de Finances pour l'année 2025 met en avant les principales orientations et objectifs liés à la gestion des effectifs publics. Il présente une vue d'ensemble des postes à pourvoir, des concours organisés, et des appels à candidatures pour des postes d'experts. Le document souligne l'importance de l'optimisation des ressources humaines dans l'administration publique, visant une meilleure gestion des compétences et des besoins en personnel, en cohérence avec les priorités budgétaires du gouvernement. Les concours et appels à candidature sont encadrés pour assurer une transparence et une efficacité dans le recrutement."""
+                st.session_state.conversation_history.add_ai_message(summary) 
+
+            else:
+                messages = [
+                {
+                    "role": "user",
+                    "content": (
+                        f"{query}. En tenant compte du texte suivant, merci de formuler une réponse en évitant de mentionner l'absence d'informations, même si certaines données manquent.  et évitez de signaler que le texte est incomplet ou fragmenté. L'objectif est de fournir une réponse claire et complète, sans critique du texte, car elle sera directement affichée au lecteur.essayer de repondre à partir de texte si tu ne trouve pas l'information dans le texte répondre d'apres votre connaissance mais focaliser sur ce texte au début essyaer de ne pas dire l'information nexiste pas si tu ne trouve pas dans le texte repondre dapres votre connaissance {text} "
+                    )
+                }
+            ]
+
+            # Appeler l'API OpenAI pour obtenir le résumé
+                response = openai.ChatCompletion.create(
+                model="gpt-4o-mini",
+                messages=messages
+            )
+
+            # Récupérer le contenu de la réponse
+
+                summary = response['choices'][0]['message']['content']
+           
+                # Votre logique pour traiter les réponses
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(response)
+                st.session_state.conversation_history.add_ai_message(summary)  # Ajouter à l'historique
+            
+            # Afficher la question et le résumé de l'assistant
+            #conversation_history.add_user_message(query)
+            #conversation_history.add_ai_message(summary)
+
+            # Format et afficher les messages comme précédemment
+                
+            # Format et afficher les messages comme précédemment
+            formatted_messages = []
+            previous_role = None 
+            if st.session_state.conversation_history.messages: # Variable pour stocker le rôle du message précédent
+                    for msg in conversation_history.messages:
+                        role = "user" if msg.type == "human" else "assistant"
+                        avatar = "🧑" if role == "user" else "🤖"
+                        css_class = "user-message" if role == "user" else "assistant-message"
+
+                        if role == "user" and previous_role == "assistant":
+                            message_div = f'<div class="{css_class}" style="margin-top: 25px;">{msg.content}</div>'
+                        else:
+                            message_div = f'<div class="{css_class}">{msg.content}</div>'
+
+                        avatar_div = f'<div class="avatar">{avatar}</div>'
+                
+                        if role == "user":
+                            formatted_message = f'<div class="message-container user"><div class="message-avatar">{avatar_div}</div><div class="message-content">{message_div}</div></div>'
+                        else:
+                            formatted_message = f'<div class="message-container assistant"><div class="message-content">{message_div}</div><div class="message-avatar">{avatar_div}</div></div>'
+                
+                        formatted_messages.append(formatted_message)
+                        previous_role = role  # Mettre à jour le rôle du message précédent
+
+                    messages_html = "\n".join(formatted_messages)
+                    st.markdown(messages_html, unsafe_allow_html=True)
+if __name__ == '__main__':
+    main()
